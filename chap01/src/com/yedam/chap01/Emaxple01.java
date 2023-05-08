903557bc76731bf48e63ae7d5145b9305a4ac602
admin <admin@YD02-12>
2023-05-08 15:27:30 +0900

    first Commit

---
+++ chap01/src/com/yedam/chap01/Emaxple01.java
@@ -0,0 +1,16 @@
+package com.yedam.chap01;
+
+public class Emaxple01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// 한 줄 주석
+		/* 
+		 * -> 여러줄 주석 
+		 * JAVA는 실행할때 주석을 Class파일에 포함X
+		 * JAVA -> main메소드에서 실행시작
+		 * main메소드가 있어야 실행된다.
+		 */ 
+		
+	}
+}
